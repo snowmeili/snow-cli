b7f49f2e13c96672a42057d096ef071a48c62815
snowmeili <545442742@qq.com>
2021-07-22 11:16:39 +0800

    feat: ceshi

---
+++ bin/cli.js
@@ -0,0 +1,60 @@
+#! /usr/bin/env node
+
+const program = require("commander");
+const create = require("../lib/create");
+const package = require("../package.json");// require可以直接拿到文件里面的内容
+const figlet = require("figlet");
+const chalk = require("chalk");
+
+program
+  .command('create <app-name>')
+  .description('create a new project')
+  .option('-f, --force', 'overwrite target directory if it exist') // 是否强制创建，当文件夹已经存在
+  .action((name, options) => {
+    // 在 create.js 中执行创建任务
+    console.log(name,options)
+    create(name, options)
+  })
+
+
+// 配置 config 命令
+program
+  .command('config [value]')
+  .description('inspect and modify the config')
+  .option('-g, --get <path>', 'get value from option')
+  .option('-s, --set <path> <value>')
+  .option('-d, --delete <path>', 'delete option from config')
+  .action((value, options) => {
+    console.log(value, options)
+  })
+  
+// 配置 ui 命令
+program
+  .command('ui')
+  .description('start add open roc-cli ui')
+  .option('-p, --port <port>', 'Port used for the UI Server')
+  .action((option) => {
+    console.log(option)
+  })
+  
+program
+  // 配置版本号信息
+  .version(`v${require('../package.json').version}`)
+  .usage('<command> [option]')
+
+program
+  .on('--help', ()=>{
+    console.log('\r\n'+ figlet.textSync('snow', {
+      font: 'Ghost',
+      horizontalLayout: 'default',
+      verticalLayout: 'default',
+      width: 80,
+      whitspaceBreak: true
+    }))
+    
+    console.log(`\r\n Run ${chalk.cyan('roc <command> --help')} show details \r\n`)
+  })
+program.parse(process.argv)
+
+// 解析用户执行命令传入参数
+program.parse(process.argv);
